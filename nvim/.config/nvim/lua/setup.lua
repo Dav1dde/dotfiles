8b041c645765d0af874cba2772f2a960c77f2c0d
David Herberth <github@dav1d.de>
2025-04-01 02:06:24 +0200

    nvim: enables treesitter and LSP based folding

--- nvim/.config/nvim/lua/setup.lua
+++ nvim/.config/nvim/lua/setup.lua
@@ -70,6 +70,21 @@ require('nvim-treesitter.configs').setup {
     },
 }
 
+vim.o.foldenable = false
+vim.o.foldmethod = 'expr'
+-- Default to treesitter folding
+vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+-- Prefer LSP folding if client supports it
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client:supports_method('textDocument/foldingRange') then
+            local win = vim.api.nvim_get_current_win()
+            vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
+        end
+    end,
+})
+
 
 function project_directory(path)
     return rooter.get_root(path) or vim.fn.expand('%:p:h')
